4ced56b3fb3050a7207e3d384250f5c4ce43a390
{bradyvolk} <{brayvanmusic@gmail.com}>
2026-05-16 22:22:15 -0500

    Add migrations for betterAuth Admin

--- backend/src/db/schema/auth-schema.ts
+++ backend/src/db/schema/auth-schema.ts
@@ -14,6 +14,10 @@ export const user = pgTable("user", {
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").default(false).notNull(),
   image: text("image"),
+  role: text("role"),
+	banned: boolean("banned"),
+	banReason: text("ban_reason"),
+	banExpires: timestamp("ban_expires", { precision: 6, withTimezone: true }),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
@@ -36,6 +40,7 @@ export const session = pgTable(
     userId: text("user_id")
       .notNull()
       .references(() => user.id, { onDelete: "cascade" }),
+    impersonatedBy: text("impersonated_by"),
     activeOrganizationId: text("active_organization_id"),
   },
   (table) => [index("session_userId_idx").on(table.userId)]
